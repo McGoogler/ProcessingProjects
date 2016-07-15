526a8071f5ea1ca0862a1995e2e4b0d36661d9ab
Mcgoogler <calebff7@gmail.com>
2016-07-15 17:19:27 +1000

    Added binary_grid project

---
+++ binary_grid/application.windows32/source/binary_grid.java
@@ -0,0 +1,87 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class binary_grid extends PApplet {
+
+
+
+public void setup() {
+  
+  background(0);
+  noLoop();
+  noFill();
+  stroke(255);
+  //frameRate(1);
+}
+
+
+public void draw()
+{
+  translate(width/2, height/2);
+  Ysystem (0, 0, 0, 10, 30, 10);
+}
+
+
+public void Ysystem (float sx, float sy, float ex, float ey, int angle, int level)
+{
+  int new_level = level-1;  
+  line(sx, sy, ex, ey);
+  //rectMode(CENTER);
+  //rect(sx,sy,10,10);
+  ellipse(sx,sy,10,10);
+  
+  if (level>0){
+    float dist = sqrt( (sx-ex)*(sx-ex)+(sy-ey)*(sy-ey) );
+    float dx = (ex-sx)/dist;
+    float dy= (ey-sy)/dist;
+    //float R = radians(angle);
+    
+    //float new_dist = 0.9*dist*cos(R);
+    float new_dist = dist;//*cos(R);
+    
+    float new_cx = ex+dx*new_dist;
+    float new_cy = ey+dy*new_dist;
+    
+    //float final_dist = 0.9*dist*sin(R);
+    float final_dist = dist;//*sin(R);
+    
+    float dx1 = -dy;
+    float dy1 = dx;
+    float dx2 = dy;
+    float dy2 = -dx;
+    
+    
+    float new_ex1 = new_cx+dx1*final_dist;
+    float new_ey1 = new_cy+dy1*final_dist;
+    float new_ex2 = new_cx+dx2*final_dist;
+    float new_ey2 = new_cy+dy2*final_dist;
+    
+    
+    Ysystem(ex, ey, new_ex1, new_ey1, angle, new_level);
+    Ysystem(ex, ey, new_ex2, new_ey2, angle, new_level);
+  }
+  return;
+  
+  
+}
+  public void settings() {  size(1366, 768); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "binary_grid" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
